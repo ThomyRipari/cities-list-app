835501841c1bdba74888fbe6296e8037fc8ac038
Thomas <thomy.ripari@outlook.com>
2019-07-30 03:57:46 -0300

    List all Cities

--- src/components/Cities/Cities.js
+++ src/components/Cities/Cities.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, TouchableWithoutFeedbacj } from 'react-native';
+import { Text, View, ScrollView, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 
 export default class Cities extends Component {
 	static navigationOptions = {
@@ -14,10 +14,43 @@ export default class Cities extends Component {
 		}
 	}
 
+	viewCity = (city) => {
+		this.props.navigation.navigate('City', {city})
+	}
+
 	render() {
-		console.log(this.props.screenProps);
 		return (
-			<Text>HELLO FROM CITIES</Text>
+			<ScrollView>
+				<View>
+					
+					{this.props.screenProps.cities.map((city, index) => (
+						<View key={index}>
+							<TouchableWithoutFeedback onPress={() => this.viewCity(city)}>
+								<View style={styles.cityContainer}>
+									<Text style={styles.city}>{city.city}</Text>
+									<Text style={styles.country}>{city.country}</Text>
+								</View>
+							</TouchableWithoutFeedback>
+						</View>
+					))}
+					
+				</View>
+			</ScrollView>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	cityContainer: {
+		padding: 10,
+		borderBottomWidth: 2,
+		borderBottomColor: '#e8075d'
+	},
+
+	city: {
+		fontSize: 20
+	},
+	country: {
+		color: 'rgba(0, 0, 0, .5)'
+	}
+})
--- src/components/Cities/City.js
+++ src/components/Cities/City.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 export default class City extends Component {
+	static navigationOptions = (props) => {
+		return {
+			title: props.navigation.state.params.city.city,
+
+			headerTitleStyle: {
+				fontSize: 20,
+				fontWeight: '400',
+				textAlign: 'center',
+				flex: 1
+			}
+		}
+	}
+
 	render() {
 		return (
 			<Text>HELLO FROM CITY</Text>
