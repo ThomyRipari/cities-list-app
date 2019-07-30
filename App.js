318138c6ac88cb434cc9309a61bb7f4a4f744ba1
Thomas <thomy.ripari@outlook.com>
2019-07-30 00:50:29 -0300

    Init React-Native App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>HELLO WORLD</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
